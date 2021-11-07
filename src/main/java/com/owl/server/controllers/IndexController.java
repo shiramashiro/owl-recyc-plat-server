ac76700b9598e5b7942ef4d8b19ff21f8d0402cc
shiramashiro <zhengrenfu@outlook.com>
2021-11-07 18:41:15 +0800

    评论接口开发完成。

--- src/main/java/com/owl/server/controllers/IndexController.java
+++ src/main/java/com/owl/server/controllers/IndexController.java
@@ -2,9 +2,12 @@ package com.owl.server.controllers;
 
 import com.owl.server.mappers.IndexMapper;
 import com.owl.server.models.BookModel;
+import com.owl.server.models.CommentModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin
@@ -61,4 +64,10 @@ public class IndexController {
         return mapper.findByType(type);
     }
 
+
+    @PostMapping("/publish/comment")
+    public void publishComment(@RequestBody CommentModel model) {
+        model.setCreate_date(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        mapper.publishComment(model);
+    }
 }
--- src/main/java/com/owl/server/mappers/IndexMapper.java
+++ src/main/java/com/owl/server/mappers/IndexMapper.java
@@ -1,6 +1,8 @@
 package com.owl.server.mappers;
 
 import com.owl.server.models.BookModel;
+import com.owl.server.models.CommentModel;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -21,4 +23,6 @@ public interface IndexMapper {
     @Select("SELECT * FROM books WHERE type = #{type}")
     List<BookModel> findByType(@Param("type") String type);
 
+    @Insert("INSERT INTO comments(book_id, user_id, content, create_date) VALUES(#{book_id}, #{user_id}, #{content}, #{create_date})")
+    void publishComment(CommentModel model);
 }
